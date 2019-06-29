9db62754dd05121b7ebe0ff228491068181b9c98
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2019-06-29 12:04:46 -0700

    Initial Commit

---
+++ URLSessionJSONRequests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSessionJSONRequests
+//
+//  Created by Georgi Malkhasyan on 6/29/19.
+//  Copyright © 2019 Malkhasyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
