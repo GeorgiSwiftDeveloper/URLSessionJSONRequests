0b1ac00dbcc9a56646b3b04eb68ac68d13f88332
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2019-06-30 01:54:57 -0700

    JSON GET & POST

--- URLSessionJSONRequests/ViewController.swift
+++ URLSessionJSONRequests/ViewController.swift
@@ -15,6 +15,45 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onGetTapped(_ sender: Any) {
+//        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {return}
+//        let session = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            if let response = response {
+//                print(response)
+//            }
+//            if let data = data {
+//                print(data)
+//                do{
+//                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+//                }catch{
+//
+//                }
+//            }
+//        }.resume()
+    }
+    
+    @IBAction func onPostTapped(_ sender: Any) {
+        let parameters = ["username": "@georgi_mal", "login": "malkhasyan"]
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        request.httpBody = httpBody
+        let session = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                }catch{
+                    print(error)
+                }
+            }
+        }.resume()
+        
+    }
 }
 
